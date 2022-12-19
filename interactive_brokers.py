c34de76ad8ab8257e889debffa2293ee09d44c93
gergelyfazekas <ffazekas.gergely@gmail.com>
2022-12-19 17:17:06 +0100

    error handling implemented

--- interactive_brokers.py
+++ interactive_brokers.py
@@ -4,6 +4,8 @@
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
 from ibapi.contract import Contract
+from ibapi.order import Order
+from ibapi.ticktype import TickTypeEnum
 
 import threading
 import time
@@ -13,22 +15,28 @@ import pandas as pd
 
 
 
-
 class IBapi(EWrapper, EClient):
     def __init__(self):
         EClient.__init__(self, self)
+        self.error = self.error
         self.data = None
         self.req_id = 0
 
-    # def error(self, reqId, errorCode, errorString):
-    #     if errorCode == 202:
-    #         print('order canceled')
+    @staticmethod
+    def error(self, reqId, errorCode, errorString):
+        if errorCode == 202:
+            print('order canceled')
 
     def tickPrice(self, reqId, tickType, price, attrib):
+        print('ticker id: ', reqId, 'tickType: ', TickTypeEnum.to_str(tickType), 'price: ', price)
         if tickType == 67 and reqId == self.req_id:
             self.data = price
-        # if tickType == 68:
-            # print('The current last price is: ', price)
+
+    # def tickSize(self, tickerId, field, size):
+    #     print("tickSize", size, tickerId, field)
+
+    # if tickType == 68:
+    # print('The current last price is: ', price)
 
     def waiting_for_result(self):
         if self.data is not None:
@@ -39,6 +47,24 @@ class IBapi(EWrapper, EClient):
 
     def run_loop(self):
         self.run()
+
+
+
+def create_order(direction, quantity, order_type, limit_price=None):
+    """creates the order object:
+    args:
+    direction: 'BUY' or 'SELL',
+    quantity: int or float, quantity to buy or sell
+    order_type: 'LMT' for limit or 'MKT' for market order
+    limit_price: optional float, only considered if order_type == 'LMT'
+    """
+    # Create order object
+    order = Order()
+    order.action = 'BUY'
+    order.totalQuantity = 100000
+    order.orderType = 'LMT'
+    order.lmtPrice = '1.10'
+    return order
 
 
 def create_contract(ticker):
@@ -66,7 +92,7 @@ def get_price_interactive_brokers(contract_list):
     app.connect('127.0.0.1', 4002, 123)
 
     ticker_list = [item.symbol for item in contract_list]
-    result_dict = dict(zip(ticker_list, [np.nan]*len(ticker_list)))
+    result_dict = dict(zip(ticker_list, [np.nan] * len(ticker_list)))
 
     # Start the socket in a thread
     api_thread = threading.Thread(target=app.run_loop, daemon=True)
@@ -78,7 +104,7 @@ def get_price_interactive_brokers(contract_list):
     for contract in contract_list:
         # Request Market Data
         app.req_id = req_id
-        app.reqMarketDataType(3)
+        app.reqMarketDataType(4)
         app.reqMktData(req_id, contract, '', False, False, [])
         result = app.waiting_for_result()
         app.data = None
@@ -87,6 +113,7 @@ def get_price_interactive_brokers(contract_list):
 
     app.disconnect()
     return result_dict
+
 
 
 if __name__ == "__main__":
